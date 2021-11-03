52fce612441d5f9a0b6befef7fb03646298412b3
Dominik Schiller <dominik.schiller@me.com>
2021-11-03 15:05:10 +0100

    Add readme

--- nova_server/tests/examples.py
+++ nova_server/tests/examples.py
@@ -1,8 +1,8 @@
-from nova_server.nova_backend import app
+from nova_server.nova_backend import create_server
 from flask import json
 
 def example_train():
-
+    app = create_server()
     response = app.test_client().post(
         '/train',
         data={
@@ -12,6 +12,22 @@ def example_train():
     )
     data = json.loads(response.get_data(as_text=True))
     print(data)
+
+def example_thread():
+    app = create_server()
+
+    def thread_test():
+        response = app.test_client().post(
+            '/thread',
+            data={
+                'file': 'a',
+            },
+            content_type='multipart/form-data'
+        )
+        data = json.loads(response.get_data(as_text=True))
+        print(data)
+
+    thread_test()
 
 if __name__ == '__main__':
     example_train()
