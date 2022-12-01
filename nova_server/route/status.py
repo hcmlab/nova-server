1927a4cd30cc4cd04eeb2f3ff4d5f751c091a8f4
Dominik Schiller <shelm@users.noreply.github.com>
2022-12-01 11:00:46 +0100

    Refactor job keys

--- nova_server/route/status.py
+++ nova_server/route/status.py
@@ -8,15 +8,11 @@ status = Blueprint("status", __name__)
 @status.route("/job_status", methods=["POST"])
 def job_status():
     if request.method == "POST":
-        request_form = request .form.to_dict()
+        request_form = request.form.to_dict()
         status_key = get_key_from_request_form(request_form)
 
         if status_key in JOBS.keys():
             status = JOBS[status_key].status
-            # TODO WTF
-            if isinstance(status, str):
-                status = JobStatus.FINISHED
-
             return jsonify({"status": status.value})
         else:
             return jsonify({"status": JobStatus.WAITING.value})
