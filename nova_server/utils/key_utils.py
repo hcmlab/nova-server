7758033be7a1586808385dad78d7126743268b63
Tobias Baur <tobias.baur@uni-a.de>
2022-12-02 23:11:00 +0100

    updated log to work with multiple sessions

--- nova_server/utils/key_utils.py
+++ nova_server/utils/key_utils.py
@@ -1,6 +1,7 @@
 
 
 def get_key_from_request_form(request_form):
-    return request_form['username'] + '_' + request_form['database'] + '_' + request_form['scheme'] + '_' + \
-           request_form['streamName'] + '_' + request_form['annotator'] + '_' + \
-           request_form['sessions']
+    serverkey = request_form['username'] + '_' + request_form['database'] + '_' + request_form['scheme'] + '_' + \
+        request_form['streamName'] + '_' + request_form['annotator'] + '_' + \
+        request_form['sessions'].replace(";", "_")
+    return serverkey[:128]    
