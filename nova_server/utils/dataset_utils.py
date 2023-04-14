53c48f0443019d11c671cac9ff6dcdbd659ad17b
dominik <dominik.schiller@me.com>
2023-04-14 14:56:21 +0200

    Fix request_form access with default values

--- nova_server/utils/dataset_utils.py
+++ nova_server/utils/dataset_utils.py
@@ -1,4 +1,7 @@
-from hcai_datasets.hcai_nova_dynamic.hcai_nova_dynamic_iterable import HcaiNovaDynamicIterable
+from hcai_datasets.hcai_nova_dynamic.hcai_nova_dynamic_iterable import (
+    HcaiNovaDynamicIterable,
+)
+
 
 def dataset_from_request_form(request_form, data_dir):
     """
@@ -6,10 +9,10 @@ def dataset_from_request_form(request_form, data_dir):
     :param request_form: the requestform that specifices the parameters of the dataset
     """
     db_config_dict = {
-        'ip': request_form["server"].split(':')[0],
-        'port': int(request_form["server"].split(':')[1]),
-        'user': request_form["username"],
-        'password': request_form["password"]
+        "ip": request_form["server"].split(":")[0],
+        "port": int(request_form["server"].split(":")[1]),
+        "user": request_form["username"],
+        "password": request_form["password"],
     }
 
     flattenSamples = False
@@ -20,16 +23,22 @@ def dataset_from_request_form(request_form, data_dir):
         # Database Config
         db_config_path=None,  # os.path.join(os.path.dirname(os.path.abspath(__file__)), 'db.cfg'),
         db_config_dict=db_config_dict,
-
         # Dataset Config
         dataset=request_form.get("database"),
         nova_data_dir=data_dir,
-        sessions=request_form.get("sessions").split(';') if request_form.get("sessions") else None,
-        roles=request_form.get("roles", '').split(';') if request_form.get("roles") else None,
-        schemes=request_form.get("scheme", '').split(';') if request_form.get("scheme") else None,
+        sessions=request_form.get("sessions").split(";")
+        if request_form.get("sessions")
+        else None,
+        roles=request_form.get("roles", "").split(";")
+        if request_form.get("roles")
+        else None,
+        schemes=request_form.get("scheme", "").split(";")
+        if request_form.get("scheme")
+        else None,
         annotator=request_form.get("annotator"),
-        data_streams=request_form.get("streamName", '').split(' ') if request_form.get("streamName") else None,
-
+        data_streams=request_form.get("streamName", "").split(" ")
+        if request_form.get("streamName")
+        else None,
         # Sample Config
         frame_size=request_form.get("frameSize"),
         stride=request_form.get("stride"),
@@ -37,13 +46,13 @@ def dataset_from_request_form(request_form, data_dir):
         right_context=request_form.get("rightContext"),
         start=request_form.get("startTime"),
         end=request_form.get("endTime"),
-
-        #TODO: This does not work with pytorch bridge when set to true because the data field does not contain the role anymore<.
+        # TODO: This does not work with pytorch bridge when set to true because the data field does not contain the role anymore<.
         # transformation cannot be applied. fix it!
         flatten_samples=flattenSamples,
-        supervised_keys=[request_form["streamName"].split(' ')[0],
-                         request_form["scheme"].split(';')[0]],
-
+        supervised_keys=[
+            request_form.get("streamName", "").split(" ")[0],
+            request_form.get("scheme", "").split(";")[0],
+        ],
         # Additional Config
         clear_cache=True,
     )
