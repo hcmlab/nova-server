825de8a0afb3cd3c70add7c4d8a4c9960558525e
dominik <dominik.schiller@me.com>
2023-09-08 17:38:09 +0200

    Update predict.py to write annotations to database

--- nova_server/exec/predict.py
+++ nova_server/exec/predict.py
@@ -7,6 +7,8 @@ import json
 from pathlib import Path, PureWindowsPath
 from nova_utils.utils import ssi_xml_utils
 from nova_utils.data.provider.nova_iterator import NovaIterator
+from nova_utils.data.handler.mongo_handler import AnnotationHandler
+from importlib.machinery import SourceFileLoader
 
 
 if __name__ == '__main__':
@@ -43,268 +45,95 @@ if __name__ == '__main__':
     parser.add_argument('--trainer_file_path', type=str, required=True, help='Path to the trainer file using Windows UNC-Style')
 
 
-    args, unknown = parser.parse_known_args()
+    # parse arguments
+    args, _ = parser.parse_known_args()
 
-    # Get all attributes that are inherited from nova_iterator_parser
-    nova_iterator_attrs = {k: v for k, v in vars(args).items() if hasattr(nova_db_parser, k)}
+    # create argument groups
+    db_args, _ = nova_db_parser.parse_known_args()
+    iter_args, _ = nova_iterator_parser.parse_known_args()
+    module_args, _ = nova_server_module_parser.parse_known_args()
 
-    # Now, nova_iterator_attrs contains all the attributes from nova_iterator_parser
-    print(nova_iterator_attrs)
-
-    # Load trainer
+    # load trainer
     trainer = ssi_xml_utils.Trainer()
-    trainer_file_path = Path(args.cml_dir).joinpath(PureWindowsPath(args.trainer_file_path))
+    trainer_file_path = Path(module_args.cml_dir).joinpath(PureWindowsPath(args.trainer_file_path))
     if not trainer_file_path.is_file():
         raise FileNotFoundError(f"Trainer file not available: {trainer_file_path}")
     else:
         trainer.load_from_file(trainer_file_path)
         print("Trainer successfully loaded.")
 
-    # Load module
+    # load module
     if not trainer.model_script_path:
         raise ValueError('Trainer has no attribute "script" in model tag.')
 
-    # Build data loaders
+    # build data loaders
     try:
-        print("Data loading")
-        sessions = args.sessions
+        sessions = iter_args.sessions
         iterators = []
 
-        args = {}
-        args |= vars(nova_db_parser.parse_known_args()[0])
-        args |= vars(nova_iterator_parser.parse_known_args()[0])
+        # TODO split for role if multirole input is false
+
+        args = {**vars(db_args), **vars(iter_args)}
 
         for session in sessions:
             print(session)
+            args['sessions'] = [session]
             ni = NovaIterator(
                 **args
             )
-            print(next(ni))
-
-            # ni = NovaIterator(
-            #     ip=args.ip.split(':')[0],
-            #     port=args.ip.split(':')[1],
-            #     user=args.user
-            # )
-        #     request_form["sessions"] = session
-        #     if trainer.model_multi_role_input:
-        #         request_form["roles"] = ";".join(roles)
-        #         iterators.append(
-        #             dataset_utils.dataset_from_request_form(request_form, data_dir)
-        #         )
-        #     else:
-        #         for role in roles:
-        #             request_form["roles"] = role
-        #             iterators.append(
-        #                 dataset_utils.dataset_from_request_form(request_form, data_dir)
-        #             )
-        # logger.info("Data iterators initialized.")
+            iterators.append(ni)
+        print("Data iterators initialized")
     except Exception as e:
-        print(e)
+        # TODO handle execption
+        raise e
 
-#####
-#####
-#####
-#####
-#
-# import copy
-# import os
-# import json
-# import sys
-# from pathlib import Path, PureWindowsPath
-# from nova_server.utils import db_utils
-# from nova_utils.utils.ssi_xml_utils import Trainer
-# from importlib.machinery import SourceFileLoader
-# from nova_server.utils.status_utils import update_progress
-# from nova_server.utils.key_utils import get_key_from_request_form
-# from nova_server.utils import (
-#     status_utils,
-#     log_utils,
-#     dataset_utils,
-#     import_utils,
-# )
-# from hcai_datasets.hcai_nova_dynamic.hcai_nova_dynamic_iterable import (
-#     HcaiNovaDynamicIterable,
-# )
-# from nova_utils.interfaces.server_module import Trainer as iTrainer
-#
-# # Check if an argument was provided
-# if len(sys.argv) != 2:
-#     print("Usage: python your_script.py 'json_string'")
-#     sys.exit(1)
-#
-# # Get the JSON string from the command line argument
-# json_str = sys.argv[1]
-#
-# # Parse the JSON string into a dictionary
-# try:
-#     my_dict = json.loads(json_str)
-#     print("Parsed Dictionary:")
-#     print(my_dict)
-# except json.JSONDecodeError as e:
-#     print("Error parsing JSON:", e)
-#
-# key = get_key_from_request_form(request_form)
-# logger = log_utils.get_logger_for_thread(key)
-#
-# cml_dir = os.environ["NOVA_CML_DIR"]
-# data_dir = os.environ["NOVA_DATA_DIR"]
-#
-# log_conform_request = dict(request_form)
-# log_conform_request["password"] = "---"
-#
-# logger.info("Action 'Predict' started.")
-# status_utils.update_status(key, status_utils.JobStatus.RUNNING)
-# trainer_file_path = Path(cml_dir).joinpath(
-#     PureWindowsPath(request_form["trainerFilePath"])
-# )
-# trainer = Trainer()
-#
-# if not trainer_file_path.is_file():
-#     logger.error(f"Trainer file not available: {trainer_file_path}")
-#     status_utils.update_status(key, status_utils.JobStatus.ERROR)
-#     return None
-# else:
-#     trainer.load_from_file(trainer_file_path)
-#     logger.info("Trainer successfully loaded.")
-#
-# if not trainer.model_script_path:
-#     logger.error('Trainer has no attribute "script" in model tag.')
-#     status_utils.update_status(key, status_utils.JobStatus.ERROR)
-#     return None
-#
-# # Load data
-# try:
-#     update_progress(key, "Data loading")
-#     sessions = request_form.pop("sessions").split(";")
-#     roles = request_form.pop("roles").split(";")
-#     iterators = []
-#     for session in sessions:
-#         request_form["sessions"] = session
-#         if trainer.model_multi_role_input:
-#             request_form["roles"] = ";".join(roles)
-#             iterators.append(
-#                 dataset_utils.dataset_from_request_form(request_form, data_dir)
-#             )
-#         else:
-#             for role in roles:
-#                 request_form["roles"] = role
-#                 iterators.append(
-#                     dataset_utils.dataset_from_request_form(request_form, data_dir)
-#                 )
-#
-#     logger.info("Data iterators initialized.")
-# except ValueError as e:
-#     print(e)
-#     log_utils.remove_log_from_dict(key)
-#     logger.error("Not able to load the data from the database!")
-#     status_utils.update_status(key, status_utils.JobStatus.ERROR)
-#     return None
-#
-# # Load Trainer
-# model_script_path = (trainer_file_path.parent / PureWindowsPath(trainer.model_script_path)).resolve()
-# if (p := (model_script_path.parent / 'requirements.txt')).exists():
-#     with open(p) as f:
-#         import_utils.assert_or_install_dependencies(
-#             f.read().splitlines(), Path(model_script_path).stem
-#         )
-#     source = SourceFileLoader(
-#         "ns_cl_" + model_script_path.stem, str(model_script_path)
-#     ).load_module()
-# # TODO: remove else block once every script is migrated to requirements.txt
-# else:
-#     source = SourceFileLoader(
-#         "ns_cl_" + model_script_path.stem, str(model_script_path)
-#     ).load_module()
-#     import_utils.assert_or_install_dependencies(
-#         source.REQUIREMENTS, Path(model_script_path).stem
-#     )
-# logger.info(f"Trainer module {Path(model_script_path).name} loaded")
-# trainer_class = getattr(source, trainer.model_create)
-# predictor = trainer_class(logger, log_conform_request)
-# logger.info(f"Model {trainer.model_create} created")
-# # model_script = source.TrainerClass(ds_iter, logger, request_form)
-#
-# # If the module implements the Trainer interface load weights
-# if isinstance(predictor, iTrainer):
-#     # Load Model
-#     model_weight_path = (
-#             trainer_file_path.parent / trainer.model_weights_path
-#     )
-#     logger.info(f"Loading weights from {model_weight_path}")
-#     predictor.load(model_weight_path)
-#     logger.info("Model loaded.")
-#
-# # Iterate over all sessions
-# ds_iter: HcaiNovaDynamicIterable
-# for ds_iter in iterators:
-#     # TODO: Remove prior creation of separate iterators to reduce redundancy
-#     ss_ds_iter = ds_iter.to_single_session_iterator()
-#
-#     logger.info("Predict data...")
-#     try:
-#         data = predictor.process_data(ss_ds_iter)
-#         annos = predictor.to_anno(data)
-#     except Exception as e:
-#         logger.error(str(e))
-#         status_utils.update_status(key, status_utils.JobStatus.ERROR)
-#         raise e
-#     logger.info("...done")
-#
-#     logger.info("Saving predictions to database...")
-#
-#     # TODO: Refactor to not use request form in upload
-#     request_form_copy = copy.copy(request_form)
-#     assert len(ss_ds_iter.sessions) == 1
-#     request_form_copy['sessions'] = ss_ds_iter.sessions[0]
-#
-#     for anno in annos:
-#         db_utils.write_annotation_to_db(request_form_copy, anno, logger)
-#     logger.info("...done")
-#
-# logger.info("Prediction completed!")
-# status_utils.update_status(key, status_utils.JobStatus.FINISHED)
+    # load Trainer
+    try:
+        model_script_path = (trainer_file_path.parent / PureWindowsPath(trainer.model_script_path)).resolve()
+        source = SourceFileLoader(
+            "ns_cl_" + model_script_path.stem, str(model_script_path)
+        ).load_module()
+        print(f"Trainer module {Path(model_script_path).name} loaded")
+    except Exception as e:
+        # TODO handle execption
+        raise e
 
+    trainer_class = getattr(source, trainer.model_create)
+    predictor = trainer_class()
+    print(f"Model {trainer.model_create} created")
 
-#
-# '''Keep for later reference to implement polygons'''
-# # model_script.ds_iter = ds_iter
-# # model_script.request_form["sessions"] = session
-# # model_script.request_form["roles"] = role
-# #
-# # logger.info("Execute preprocessing.")
-# # model_script.preprocess()
-# # logger.info("Preprocessing done.")
-# #
-# # logger.info("Execute prediction.")
-# # model_script.predict()
-# # logger.info("Prediction done.")
-# #
-# # logger.info("Execute postprocessing.")
-# # results = model_script.postprocess()
-# # logger.info("Postprocessing done.")
-# #
-# # logger.info("Execute saving process.")
-# # db_utils.write_annotation_to_db(request_form, results, logger)
-# # logger.info("Saving process done.")
-#
-# # 5. In CML case, delete temporary files..
-# # if request_form["deleteFiles"] == "True":
-# #     trainer_name = request_form["trainerName"]
-# #     logger.info("Deleting temporary CML files...")
-# #     out_dir = Path(cml_dir).joinpath(
-# #         PureWindowsPath(request_form["trainerOutputDirectory"])
-# #     )
-# #     os.remove(out_dir / trainer.model_weights_path)
-# #     os.remove(out_dir / trainer.model_script_path)
-# #     for f in model_script.DEPENDENCIES:
-# #         os.remove(trainer_file_path.parent / f)
-# #     trainer_fullname = trainer_name + ".trainer"
-# #     os.remove(out_dir / trainer_fullname)
-# #     logger.info("...done")
-#
-# # except Exception as e:
-# # logger.error('Error:' + str(e))
-# #   status_utils.update_status(key, status_utils.JobStatus.ERROR)
-# # finally:
-# #    del results, ds_iter, ds_iter_pp, model, model_script, model_script_path, model_weight_path, spec
+    # TODO implement SSI interface
+    # If the module implements the Trainer interface load weights
+    # if isinstance(predictor, iTrainer):
+    #     # Load Model
+    #     model_weight_path = (
+    #             trainer_file_path.parent / trainer.model_weights_path
+    #     )
+    #     logger.info(f"Loading weights from {model_weight_path}")
+    #     predictor.load(model_weight_path)
+    #     logger.info("Model loaded.")
+
+    # Init database handler
+    db_handler = AnnotationHandler(**vars(db_args))
+
+    # Iterate over all sessions
+    for ds_iter in iterators:
+
+        print(f"Predict session {ds_iter.sessions[0]}...")
+        try:
+            data = predictor.process_data(ds_iter)
+            annos = predictor.to_anno(data)
+        except FileNotFoundError as e:
+            print(f"Iterator exited with error: '{str(e)}'. Continuing.")
+
+        print("...done")
+        print("Saving predictions to database...")
+
+        for anno in annos:
+            db_handler.save(
+                anno
+            )
+
+        print("...done")
+
+    print("Prediction completed!")
