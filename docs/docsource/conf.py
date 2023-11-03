a16c87a0f31c813723dd413ef5f981fb9ca5c764
dominik <dominik.schiller@me.com>
2023-11-03 09:59:07 +0100

    Update conf.py

--- docs/docsource/conf.py
+++ docs/docsource/conf.py
@@ -1,6 +1,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 import nova_server
 
 # Configuration file for the Sphinx documentation builder.
