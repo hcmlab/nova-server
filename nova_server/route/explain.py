e84891f10ea8b4a53d0d75eff27d6ddb6910397d
Alex Heimerl <alexander-heimerl@t-online.de>
2023-11-16 14:33:27 +0100

    Update explain.py

--- nova_server/route/explain.py
+++ nova_server/route/explain.py
@@ -26,34 +26,7 @@ from lime.lime_tabular import LimeTabularExplainer
 import tf_explain
 import dice_ml
 
-
-
-def prepare_image(image, target):
-    # if the image mode is not RGB, convert it
-    if image.mode != "RGB":
-        image = image.convert("RGB")
-    # resize the input image and preprocess it
-    image = image.resize(target)
-    image = tf.keras.preprocessing.image.img_to_array(image)
-    image = np.expand_dims(image, axis=0)
-
-    # return the processed image
-    return image
-
-def getTopXpredictions(prediction, topLabels):
-
-    prediction_class = []
-
-    for i in range(0, len(prediction[0])):
-        prediction_class.append((i, prediction[0][i]))
-
-    prediction_class.sort(key=lambda x: x[1], reverse=True)
-
-    return prediction_class[:topLabels]
-
-
 explain = Blueprint("explain", __name__)
-
 
 @explain.route("/explain", methods=["POST"])
 def explain_thread():
@@ -66,19 +39,6 @@ def explain_thread():
             THREADS[job_id] = thread
             data = {"success": str(job_added)}
             return jsonify(data)
-
-            
-            explanation_framework = request_form.get("explainer")
-
-            if explanation_framework == "LIME_IMAGE":
-                return lime_image(request_form)
-            elif explanation_framework == "LIME_TABULAR":
-                return lime_tabular(request_form)
-            elif explanation_framework == "DICE":
-                return dice(request_form)
-            elif explanation_framework == "TF_EXPLAIN":
-                return tf_explainer(request_form)
-            return None
 
 
 @thread_utils.ml_thread_wrapper
@@ -97,110 +57,3 @@ def explain_data(request_form, job_id):
     except Exception as e:
         logger.critical(f"Job failed with exception {str(e)}")
         job_utils.update_status(job_id, job_utils.JobStatus.ERROR)
-
-def tf_explainer(request):
-    data = {"success": "failed"}
-
-    frame = request.get("frame")
-    explainer = request.get("tfExplainer")
-
-    nova_iterator = NovaIterator(
-        request.get("dbHost"),
-        int(request.get("dbPort")),
-        request.get("dbUser"),
-        request.get("dbPassword"),
-        request.get("dataset"),
-        os.environ["NOVA_SERVER_DATA_DIR"],
-        ast.literal_eval(request.get("sessions")),
-        ast.literal_eval(request.get("data")),
-        0
-        )
-
-    # NOTE: loading model before initializing data results in winerror insufficient system resources
-    session = nova_iterator.init_session(ast.literal_eval(request.get("sessions"))[0])
-    sample = session.input_data["explanation_stream"].data[frame]
-
-    model_path = request.get("modelPath")
-    model = tf.keras.models.load_model(model_path)
-
-
-    image = prepare_image(Image.fromarray(sample), (224, 224))
-    image = image * (1.0 / 255)
-    prediction = model.predict(image)
-    topClass = getTopXpredictions(prediction, 1)
-    print(topClass[0])
-    image = np.squeeze(image)
-
-    if explainer == "GRADCAM":
-        im = ([image], None)
-        from tf_explain.core.grad_cam import GradCAM
-
-        exp = GradCAM()
-        imgFinal = exp.explain(im, model, class_index=topClass[0][0])
-        # exp.save(imgFinal, ".", "grad_cam.png")
-
-    elif explainer == "OCCLUSIONSENSITIVITY":
-        im = ([image], None)
-        from tf_explain.core.occlusion_sensitivity import OcclusionSensitivity
-
-        exp = OcclusionSensitivity()
-        imgFinal = exp.explain(
-            im, model, class_index=topClass[0][0], patch_size=10
-        )
-        # exp.save(imgFinal, ".", "grad_cam.png")
-
-    elif explainer == "GRADIENTSINPUTS":
-        im = (np.array([image]), None)
-        from tf_explain.core.gradients_inputs import GradientsInputs
-
-        exp = GradientsInputs()
-        imgFinal = exp.explain(im, model, class_index=topClass[0][0])
-        # exp.save(imgFinal, ".", "gradients_inputs.png")
-
-    elif explainer == "VANILLAGRADIENTS":
-        im = (np.array([image]), None)
-        from tf_explain.core.vanilla_gradients import VanillaGradients
-
-        exp = VanillaGradients()
-        imgFinal = exp.explain(im, model, class_index=topClass[0][0])
-        # exp.save(imgFinal, ".", "gradients_inputs.png")
-
-    elif explainer == "SMOOTHGRAD":
-        im = (np.array([image]), None)
-        from tf_explain.core.smoothgrad import SmoothGrad
-
-        exp = SmoothGrad()
-        imgFinal = exp.explain(im, model, class_index=topClass[0][0])
-        # exp.save(imgFinal, ".", "gradients_inputs.png")
-
-    elif explainer == "INTEGRATEDGRADIENTS":
-        im = (np.array([image]), None)
-        from tf_explain.core.integrated_gradients import IntegratedGradients
-
-        exp = IntegratedGradients()
-        imgFinal = exp.explain(im, model, class_index=topClass[0][0])
-        # exp.save(imgFinal, ".", "gradients_inputs.png")
-
-    elif explainer == "ACTIVATIONVISUALIZATION":
-        # need some solution to find out and submit layers name
-        im = (np.array([image]), None)
-        from tf_explain.core.activations import ExtractActivations
-
-        exp = ExtractActivations()
-        imgFinal = exp.explain(im, model, layers_name=["activation_1"])
-        # exp.save(imgFinal, ".", "gradients_inputs.png")
-
-    img = pilimage.fromarray(imgFinal)
-    imgByteArr = inputoutput.BytesIO()
-    img.save(imgByteArr, format="JPEG")
-    imgByteArr = imgByteArr.getvalue()
-
-    img64 = base64.b64encode(imgByteArr)
-    img64_string = img64.decode("utf-8")
-
-    data["explanation"] = img64_string
-    data["prediction"] = str(topClass[0][0])
-    data["prediction_score"] = str(topClass[0][1])
-    data["success"] = "success"
-
-    return json.dumps(data)
